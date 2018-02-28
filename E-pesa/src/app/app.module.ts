484ea7f74d5d46888beb4a81b39f306772249069
waploaj <waploaj@icloud.com>
2018-03-01 00:25:05 +0300

    components

--- E-pesa/src/app/app.module.ts
+++ E-pesa/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { HelpComponent } from './component/help/help.component';
+import { DocumentationComponent } from './component/documentation/documentation.component';
+import { LearnmoreComponent } from './component/learnmore/learnmore.component';
+import { SignupComponent } from './component/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    HelpComponent,
+    DocumentationComponent,
+    LearnmoreComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
